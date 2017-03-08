617d971d5faafaf4dbff71956591ffbf6dcef049
Jules Rau <jules.rau@gmx.de>
2017-03-08 03:31:02 +0100

    Update checkUsername.php

--- ajax/checkUsername.php
+++ ajax/checkUsername.php
@@ -13,10 +13,22 @@ function printResponse($res, $text){
 	die();
 }
 
-$reg = new FaceRecognation($user);
+if(strlen($user) < 5)
+	printResponse(false, 'Username muss mind. 5 Zeichen lang sein.');
 
-$res = $reg->checkUser();
-if($res === false) printResponse($res, $reg->responseText);
+if(file_exists('../media/user/login/' . $user . '.jpeg'))
+	printResponse(false, 'Username bereits vergeben.');
+
+$db = new DatabaseConnection();
+
+$row = $db->query(
+	'SELECT `name` FROM `user` WHERE `name` = :user', 
+	array('user' => $user)
+);
+
+if($row)
+	printResponse(false, 'Username bereits vergeben.');
+
 
 printResponse(true, 'Username frei.');
 ?>
